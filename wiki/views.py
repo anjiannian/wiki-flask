3ffd2a9dd0d213b8a1735255fc5223fd05ea9d90
Chad Ying <anjiannian@gmail.com>
2014-07-16 03:22:56 +0800

    seperate edit/echo

--- wiki/views.py
+++ wiki/views.py
@@ -4,36 +4,49 @@
 from . import wiki_b
 from models import Wiki
 from flask import request, redirect, render_template
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_required
 
 
 @wiki_b.route("/wiki/<string:wiki_url>", methods=["GET", "POST"])
-def wiki(wiki_url):
+def echo_wiki(wiki_url):
     flag = not current_user.is_anonymous() or "None"
-    print flag
+    try:
+        item = Wiki.objects(title=wiki_url).order_by('-updated_at')[0]
+    except:
+        return redirect("/edit/"+wiki_url)
+
     if request.method == "POST":
-        title = wiki_url
-        author = current_user.username or 'Anonymous'
+        return render_template('/wiki/edit_page.html',
+                               flag=flag,
+                               title=item.title,
+                               content=item.content,
+                               author=item.author)
+
+    elif request.method == "GET":
+        return render_template('/wiki/wiki_page.html',
+                               flag=flag,
+                               title=item.title,
+                               content=item.content,
+                               author=item.author,
+                               created_at=item.created_at)
+
+
+
+@wiki_b.route("/edit/<string:wiki_url>", methods=["GET", "POST"])
+@login_required
+def edit_wiki(wiki_url):
+    title = wiki_url
+    author = current_user.username or 'Anonymous'
+    if request.method == "POST":
         content = request.form["content"]
         wiki = Wiki(title=title,
                     author=author,
                     content=content)
         wiki.save()
-        redirect("/wiki/"+title)
-    try:
-        item = Wiki.objects(title=wiki_url).order_by('-updated_at')[0]
-    except:
-        return render_template("/wiki/newpage.html",
-                               wiki_url=wiki_url,
-                               author=author)
-        if wiki_url == str(item.title):
-            return render_template('/wiki/wikipage.html',
-                                   flag=flag,
-                                   title=item.title,
-                                   content=item.content,
-                                   author=item.author,
-                                   created_at=item.created_at)
-
+        return redirect("/wiki"+title)
+    return render_template("/wiki/edit_page.html",
+                           title=title,
+                           author=author)
 
 @wiki_b.route("/")
 def index():
